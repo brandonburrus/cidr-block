07f57ab4df45a53a1321469f606b31852876e23e
Brandon Burrus <brandon@burrus.io>
2021-10-10 16:15:33 -0700

    feat: setup repo

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import pkg from './package.json'
+import typescript from '@rollup/plugin-typescript'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+      compact: true
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true,
+      compact: true
+    }
+  ],
+  external: Object.keys(pkg.dependencies),
+  plugins: [
+    typescript({
+      tsconfig: './tsconfig.json'
+    })
+  ]
+}
