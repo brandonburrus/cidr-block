cb59bf4cb51ba302db9a3960f0dfcaa81690656d
Brandon Burrus <brandon@burrus.io>
2021-10-11 12:30:27 -0700

    feat: continue ipv4 cidr block impl

---
+++ src/cidr/ipv6/constants.ts
@@ -0,0 +1,4 @@
+/**
+ * The numerical maximum size an IPv6 address can be
+ */
+export const MAX = 0xffff_ffff_ffff_ffffn
--- tests/cidr/ipv4/ipv4-address.test.ts
+++ tests/cidr/ipv4/ipv4-address.test.ts
@@ -18,14 +18,14 @@ describe('ipv4', () => {
       ['255.255.0.255', 0b11111111_11111111_00000000_11111111n],
       ['255.255.255.255', 0b11111111_11111111_11111111_11111111n]
     ])('converts strings to ipv4 numbers (%s should equal %d)', (input: string, output: bigint) => {
-      expect(cidr.ipv4.fromString(input)).toBe(output)
+      expect(cidr.ipv4.stringToNum(input)).toBe(output)
     })
 
     it('throws errors when the string is an invalid ip', () => {
-      expect(() => cidr.ipv4.fromString('')).toThrow()
-      expect(() => cidr.ipv4.fromString('-10.0.0.0')).toThrow()
-      expect(() => cidr.ipv4.fromString('255.255.-3.0')).toThrow()
-      expect(() => cidr.ipv4.fromString('255.255.256.0')).toThrow()
+      expect(() => cidr.ipv4.stringToNum('')).toThrow()
+      expect(() => cidr.ipv4.stringToNum('-10.0.0.0')).toThrow()
+      expect(() => cidr.ipv4.stringToNum('255.255.-3.0')).toThrow()
+      expect(() => cidr.ipv4.stringToNum('255.255.256.0')).toThrow()
     })
   })
 
@@ -40,12 +40,12 @@ describe('ipv4', () => {
       [0b11111111_11111111_00000000_11111111n, '255.255.0.255'],
       [0b11111111_11111111_11111111_11111111n, '255.255.255.255']
     ])('converts ipv4 numbers to strings (%d should equal %s)', (input: bigint, output: string) => {
-      expect(cidr.ipv4.toString(input)).toBe(output)
+      expect(cidr.ipv4.numToString(input)).toBe(output)
     })
 
     it('throws errors when the number is an invalid ip', () => {
-      expect(() => cidr.ipv4.toString(-1n)).toThrowError()
-      expect(() => cidr.ipv4.toString(2n ** 33n)).toThrowError()
+      expect(() => cidr.ipv4.numToString(-1n)).toThrowError()
+      expect(() => cidr.ipv4.numToString(2n ** 33n)).toThrowError()
     })
   })
 })
