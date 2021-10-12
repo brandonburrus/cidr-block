2b351ee7534e3bab1aeb7820a0580d1c987676ae
Brandon Burrus <brandon@burrus.io>
2021-10-12 08:39:33 -0700

    refactor: use number for ipv4 instead of bigint

--- src/cidr/ipv4/types.ts
+++ src/cidr/ipv4/types.ts
@@ -1,9 +1,9 @@
 import { Ipv4Address } from './ipv4-address'
 
 /**
- * Type that indicates a literal string or bigint value that represents an IPv4 address
+ * Type that indicates a literal string or number value that represents an IPv4 address
  */
-export type Ipv4Literal = string | bigint
+export type Ipv4Literal = string | number
 
 /**
  * Type that indicates either a literal value or an address instance that is an IPv4
